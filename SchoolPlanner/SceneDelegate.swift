bc36bab55cffbb905663a466811771a4cc69a990
Cory Lowry <corylowry12@gmail.com>
2021-11-02 22:18:36 -0500

    Added automatic setting of the accent color

--- SchoolPlanner/SceneDelegate.swift
+++ SchoolPlanner/SceneDelegate.swift
@@ -10,12 +10,29 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    let userDefaults = UserDefaults.standard
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        if userDefaults.integer(forKey: "accent") == 0 {
+                    window?.tintColor = UIColor(rgb: 0x347deb)
+                }
+                else if userDefaults.integer(forKey: "accent") == 1 {
+                    window?.tintColor = UIColor(rgb: 0x7841c4)
+                }
+                else if userDefaults.integer(forKey: "accent") == 2 {
+                    window?.tintColor = UIColor(rgb: 0x26A69A)
+                }
+                else if userDefaults.integer(forKey: "accent") == 3 {
+                    window?.tintColor = UIColor(rgb: 0xfc783a)
+                }
+                else if userDefaults.integer(forKey: "accent") == 4 {
+                    window?.tintColor = UIColor(rgb: 0xc41d1d)
+                }
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
